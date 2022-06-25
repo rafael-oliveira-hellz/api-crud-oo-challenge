a7794a254fe399a87fb10330c6e6e5157bd40aca
Rafael de Oliveira (Hellz) <rafael.oliveirasylva@gmail.com>
2022-06-25 00:38:13 -0300

    feat: remove unnecessary imports

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,9 +1,4 @@
-import dotenv from 'dotenv';
 import { Request, Response } from 'express';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-
-dotenv.config();
 
 // Model
 import { User } from '@src/models/User';
